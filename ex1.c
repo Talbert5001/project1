58a499aae7e5f750a84fd4f60512c7f83b56a784
Talbert <therndon1@my.gcu.edu>
2022-02-06 18:20:32 -0700

    made ex better

--- ex1.c
+++ ex1.c
@@ -7,38 +7,16 @@
 int main (int argc, char *argv[])
 {
 	
-	//fork();
+	/fork();
 	
 	printf("PID of ex1.c = %d\n", getpid());
-	//char *args[] = {"Hello","Test", "Citro", NULL};
+	char *args[] = {"Hello","Test", "Citro", NULL};
 	
 	
-	//execv("./ex2",args);
-	//printf("Back to ex1.c");
+	execv("./ex2",args);
+	printf("Back to ex1.c");
 
-	if (argc < 1);
-		return 0;
-
-	int i;
-	int strsize = 0;
-	for (i =1; i <argc; i++){
-		strsize += strlen(argv[i]);
-		if(argc > i+1)
-		strsize++;
-	}
-
-	printf("strsize: %d\n",strsize);
-
-	char *cmdstring;
-
-	cmdstring[0] = '\0';
 	
-	for (i=1; i<argc; i++){
-		strcat(cmdstring, argv[i]);
-		if(argc > i+1)
-			strcat(cmdstring," ");
-	}
-	printf("cmdstring: %s\n", cmdstring);
 
 	return 0;
 
